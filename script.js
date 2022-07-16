7b88eca17d74a4d1cb341515e3ae9aa473f4ffa8
MariIvChin <mari.chin1976@gmail.com>
2022-07-17 01:45:14 +0300

    all tasks done

--- script.js
+++ script.js
@@ -3,42 +3,39 @@
 console.dir(window.document.getElementById("fancy-ball"));
 let ballElement = window.document.getElementById("fancy-ball");
 console.dir(ballElement);
-ballElement.style.height = "70px";
-ballElement.style.width = "70px";
+ballElement.style.height = "120px";
+ballElement.style.width = "120px";
 ballElement.style.display = "inline-block";
-ballElement.style.backgroundColor = "lightgreen";
-ballElement.style.borderStyle = "dotted";
-ballElement.style.borderColor = "black";
+ballElement.style.backgroundImage =
+  "linear-gradient(100deg, green, lightgreen)";
+ballElement.style.border = "2px dashed black";
 ballElement.style.borderRadius = "50%";
 
-// var boxElement = document.getElementById("fancy-ball");
-// var ctx = ballElement.getContext("2d");
-
-// // Create gradient
-// var grd = ctx.createLinearGradient(0, 0, 200, 0);
-// grd.addColorStop(0,"red");
-// grd.addColorStop(1,"white");
-
-// // Fill with gradient
-// ctx.fillStyle = grd;
-// ctx.fillRect(10, 10, 150, 80);
-
 //List
+
 console.dir(window.document.getElementById("personal-list"));
 let listElement = window.document.getElementById("personal-list");
 console.dir(listElement);
 
 listElement.innerHTML =
-  "<ul><li>'first name: Maria'</li><li>'last name: Chin'</li><li>'age: 46'</li></ul>";
+  "<ul><strong>My Info:</strong><li>'first name: Maria'</li><li>'last name: Chin'</li><li>'age: 46'</li></ul>";
+
+listElement.style.fontSize = "20px";
+listElement.style.border = "2px inset blue";
+listElement.style.borderRadius = "5%";
+listElement.style.padding = "10px";
 
 //Danger Color
+
 console.dir(window.document.getElementById("bg-color"));
 let bgcolorElement = window.document.getElementById("bg-color");
 console.dir(bgcolorElement);
-bgcolorElement.className = "bg-danger";
+bgcolorElement.className = "bg-danger h5 pe-5 ps-5 mt-3 mb-5";
+bgcolorElement.style.color = "white";
 
 //Car Card
-let carCard = {
+
+const carCard = {
   title: "car",
   price: 200_000,
   hand: 2,
@@ -46,8 +43,33 @@ let carCard = {
   year: 2014,
 };
 
-// let cardElement = window.document.getElementById("car-card");
-// console.dir(cardElement);
-
-// cardElement.getElementsByClassName("card-title").
-// console.dir(cardElement.className);
+const cardElement = window.document.getElementById("car-card");
+cardElement.innerHTML =
+  `<div class="card" id="car-card" style="width: 18rem">
+      <img src="./tesla.jfif.crdownload" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Tesla</h5>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">type: ` +
+  carCard.title +
+  `</li>
+        <li class="list-group-item">price: ` +
+  carCard.price +
+  `</li>
+        <li class="list-group-item">hand: ` +
+  carCard.hand +
+  `</li>
+        <li class="list-group-item">km: ` +
+  carCard.km +
+  `</li>
+        <li class="list-group-item">year: ` +
+  carCard.year +
+  `</li>
+      </ul>
+      <div class="card-body">
+        <a href="https://www.tesla.com/" target="_blank" class="card-link"
+          >Tesla's site</a
+        >
+      </div>
+    </div>`;
